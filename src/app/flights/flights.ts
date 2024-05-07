254465d5fc8b81f79de8ace90dd6854dfe6e1996
ui-wizard <morton.nari@yahoo.com.au>
2024-05-07 17:33:26 +1000

    clean up

--- src/app/flights/flights.ts
+++ src/app/flights/flights.ts
@@ -1,12 +1,12 @@
 export class Flights {
-    pilot: string;
+    person: string;
     RPA: string;
     startTimeMinCount: number;
     endTimeMinCount: number;
     get duration() {
         return this.endTimeMinCount - this.startTimeMinCount};
-    constructor(pilot: string, RPA: string, startTimeMinCount: number, endTimeMinCount: number) {
-      this.pilot = pilot;
+    constructor(person: string, RPA: string, startTimeMinCount: number, endTimeMinCount: number) {
+      this.person = person;
       this.RPA = RPA;
       this.startTimeMinCount = startTimeMinCount;
       this.endTimeMinCount = endTimeMinCount;
